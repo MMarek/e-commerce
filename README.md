66692a6f962f366006d3dfbca085e5892321aec9
Marek Mildner <mildnermarek@gmail.com>
2020-05-26 11:15:08 +0200

    react app + sass

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
++ SASS
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/App.js
+++ src/App.js
@@ -1,36 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends React.Component {
-    state = {
-        meaningOfLife: 47
-    }
+import HomePage from "./hompage.comnonent";
 
-
-    handleClick = () => {
-        this.setState((prevState, prevProps) => {
-                return {meaningOfLife: prevState.meaningOfLife + this.props.increment}
-            },
-            () => console.log(this.state.meaningOfLife)
-        )
-    }
-
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <p>
-                        {this.state.meaningOfLife}
-                    </p>
-                    <button onClick={this.handleClick}>
-                        Update State
-                    </button>
-                </header>
-            </div>
-        );
-    }
+function App() {
+    return (
+        <div>
+            <HomePage/>
+        </div>
+    );
 }
 
 export default App;
