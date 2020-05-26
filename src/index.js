1d81202c31f42c45c32c595a3c5980806db52a9d
Marek Mildner <mildnermarek@gmail.com>
2020-05-26 13:48:03 +0200

    components + menu items + homepage + styling menu

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App increment={1} />
+        <App />
     </React.StrictMode>,
     document.getElementById('root')
 );
